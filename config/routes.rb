9e00e612b3af5fb43abeb21b9f6c9b41688d8e4f
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2012-12-23 17:56:52 +0200

    move :as => :topic back

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Communities::Application.routes.draw do
 
   resources :communities, path: "game" do
-    resources :community_topics, path: "topic"
+    resources :community_topics, path: "topic", as: "topic"
   end
 
   root :to => 'communities#index'
